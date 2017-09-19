cab822888074b9907b8deb64aef66d117665edcd
rushaniia.badrutdinova <rushaniia.badrutdinova@gmail.com>
2017-09-19 10:23:51 +0300

    Add files via upload

---
+++ StackBalancedBrackets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksBalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = "{[(])}";
+            if (IsBalanced(expression.ToCharArray()))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            Console.ReadLine();
+        }
+
+        static bool IsBalanced(char[] chars)
+        {
+            Stack<char> s = new Stack<char>();
+            foreach (char c in chars)
+            {
+                if (c == '{') s.Push('}');
+                else if (c == '(') s.Push(')');
+                else if (c == '[') s.Push(']');
+                else
+                {
+                    if (s.Count == 0 || c != (char)s.Peek())
+                        return false;
+                    s.Pop();
+                }
+            }
+            return s.Count == 0 ? true : false;
+        }
+    }
+}
